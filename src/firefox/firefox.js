9740ae38c0cf2287e3869f73a71f4ec8998f3378
Timu Eren <selamtux@gmail.com>
2016-06-21 20:19:52 +0300

    Update firefox.js
    added http and https links into pagemod

--- src/firefox/firefox.js
+++ src/firefox/firefox.js
@@ -2,7 +2,7 @@ var data = require('sdk/self').data
   , pageMod = require('sdk/page-mod')
 
 pageMod.PageMod({
-  include: 'http://www.eksiduyuru.com/*',
+  include: ['http://www.eksiduyuru.com/*', 'https://www.eksiduyuru.com/*', 'http://eksiduyuru.com/*', 'https://eksiduyuru.com/*'],
   contentScriptFile : [
                         data.url('jquery-2.1.1.js'),
                         data.url('troll-blocker.js'),
